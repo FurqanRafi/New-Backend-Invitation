8147054e5abc7a1fdbdacb3e51c80ef1e24a0213
FurqanRafi <140535011+FurqanRafi@users.noreply.github.com>
2025-10-06 11:17:04 +0500

    Update mongodbConnect.js

--- config/mongodbConnect.js
+++ config/mongodbConnect.js
@@ -20,7 +20,7 @@ const ConnectDb = async () => {
   if (isConnected) return;
 
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_URL, {
       dbName: "your-db-name", // optional
     });
     isConnected = true;
