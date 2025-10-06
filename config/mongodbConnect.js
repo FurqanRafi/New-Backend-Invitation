24340543a69a8554f6b42b4e42573ac9e0f9351c
Muhammad Furqan <furqanf64123@gmail.com>
2025-10-06 10:33:01 +0500

    t

--- config/mongodbConnect.js
+++ config/mongodbConnect.js
@@ -1,15 +1,34 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
+// const ConnectDb = async () =>{
+//     try {
+//         await mongoose.connect(process.env.MONGO_URL)
+//         console.log('Mongodb Connected')
+//     } catch (error){
+//         console.log(error)
+//     } finally {
 
-const ConnectDb = async () =>{
-    try {
-        await mongoose.connect(process.env.MONGO_URL)
-        console.log('Mongodb Connected')
-    } catch (error){
-        console.log(error)
-    } finally {
-        
-    }
-}
+//     }
+// }
 
-module.exports = ConnectDb
+// module.exports = ConnectDb
+
+const mongoose = require("mongoose");
+let isConnected = false;
+
+const ConnectDb = async () => {
+  if (isConnected) return;
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "your-db-name", // optional
+    });
+    isConnected = true;
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("Mongo Error:", error);
+    throw error;
+  }
+};
+
+module.exports = ConnectDb;
